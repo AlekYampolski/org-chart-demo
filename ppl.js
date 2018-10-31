3192cb4ee254853bb4cf754985489270a4ae8f82
Oleksandr Yampolskyi <34868863+AlekYampolski@users.noreply.github.com>
2018-10-31 22:56:04 +0200

    Create ppl.js
    Init

---
+++ ppl.js
@@ -0,0 +1,38 @@
+var ppl = [
+  {
+    id: "id-scott",
+    name: "scott",
+    color: "tomato",
+    groups: {
+      faculties: "faculties-1",
+      squads: "squads-1",
+      agencies: "",
+      councils: ["councils-2", "councils-3"],
+      clubs: ["clubs-1"]
+    }
+  },
+  {
+    id: "id-dread",
+    name: "dread",
+    color: "orangered",
+    groups: {
+      faculties: "",
+      squads: "squads-3",
+      agencies: "",
+      councils: ["councils-1"],
+      clubs: ["clubs-1", "clubs-2", "clubs-3"]
+    }
+  },
+  {
+    id: "id-will",
+    name: "will",
+    color: "lightgreen",
+    groups: {
+      faculties: "faculties-1",
+      squads: "squads-2",
+      agencies: "agencies-1",
+      councils: ["councils-2", "councils-2"],
+      clubs: ["clubs-1"]
+    }
+  },
+];
