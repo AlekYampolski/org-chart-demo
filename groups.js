859d38da6f2ebb56c262ee62a572c9854ef628f9
Oleksandr Yampolskyi <34868863+AlekYampolski@users.noreply.github.com>
2018-10-31 22:57:03 +0200

    Update groups.js
    Refactoring

--- groups.js
+++ groups.js
@@ -1,68 +1,122 @@
 var groups = [
   {
-    type: "room",
-    color : "red",
+    type: "squads",
+    color: "pink",
     items: [
       {
-        id: "room-1",
-        name: "Room for 1",
-        descr: {
-          x: 100,
-          y: 100,
-          r: 50
+        id: "squads-1",
+        name: "dolor ac nunc tristique",
+        position: {
+          x: 820,
+          y: 158,
+          r: 43
         }
       },
       {
-        id: "room-2",
-        name: "Room for 2",
-        descr: {
-          x: 325,
-          y: 80,
-          r: 50
+        id: "squads-2",
+        name: "elementum sapien maximus",
+        position: {
+          x: 420,
+          y: 750,
+          r: 32
         }
       },
       {
-        id: "room-3",
-        name: "Room for 2",
-        descr: {
-          x: 325,
-          y: 450,
-          r: 60
-        }
-      },
-      {
-        id: "room-4",
-        name: "Room for 2",
-        descr: {
-          x: 700,
-          y: 600,
-          r: 40
+        id: "squads-3",
+        name: "metus erat",
+        position: {
+          x: 220,
+          y: 350,
+          r: 32
         }
       },
     ]
   },
   {
-    type: "faculty",
-    color : 'aqua',
+    type: "faculties",
+    color: "green",
     items: [
       {
-        id: "faculty-1",
-        name: "faculty for 1",
-        descr: {
-          x: 150,
-          y: 370,
-          r: 50
+        id: "faculties-1",
+        name: "tempor neque",
+        position: {
+          x: 480,
+          y: 640,
+          r: 25
+        }
+      },
+    ]
+  },
+  {
+    type: "agencies",
+    color: "lightpink",
+    items: [
+      {
+        id: "agencies-1",
+        name: "quis purus rhoncus",
+        position: {
+          x: 840,
+          y: 640,
+          r: 35
+        }
+      },
+    ]
+  },
+  {
+    type: "councils",
+    color: "blue",
+    items: [
+      {
+        id: "councils-1",
+        name: "orci mi",
+        position: {
+          x: 320,
+          y: 540,
+          r: 25
         }
       },
       {
-        id: "faculty-2",
-        name: "faculty for 1",
-        descr: {
-          x: 580,
-          y: 791,
-          r: 50
+        id: "councils-2",
+        name: "sit amet imperdiet",
+        position: {
+          x: 120,
+          y: 840,
+          r: 35
         }
       },
+      {
+        id: "councils-3",
+        name: "facilisis quis purus",
+        position: {
+          x: 725,
+          y: 240,
+          r: 32
+        }
+      }
+    ]
+  },
+  {
+    type: "clubs",
+    color: "orange",
+    items: [
+      {
+        id: "clubs-1",
+        name: "Quis quam eget",
+        position: {
+          x: 100,
+          y: 100,
+          r: 18
+        }
+      },
+      {
+        id: "clubs-2",
+        name: "Malesuada eu non",
+        position: {
+          x: 245,
+          y: 725,
+          r: 30
+        }
+      }
     ]
   }
 ];
