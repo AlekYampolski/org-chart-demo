490e81ac4f60ce9df27eeb4737e2ab5cbe201523
Aleksander Yampolski <al.yampolski@gmail.com>
2018-11-04 22:47:37 +0200

    Add panel view. Init

---
+++ js/view/panel.js
@@ -0,0 +1,10 @@
+// var svgPanel = d3.select('svg#panel-1')
+//                     .attr('width', 200)
+//                     .attr('height', 200)
+//                     .style('background', panelControl.color)
+//                     .style('position', 'fixed')
+//                     .style('left', 800)
+//                     .style('top', 10);
+
+
+
